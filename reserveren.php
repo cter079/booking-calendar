b25c205e43394dfca879b6f54d73a7b8bb32a030
Camryn Terlouw <86618337+cter079@users.noreply.github.com>
2022-02-04 12:59:48 +0100

    Update reserveren.php

--- reserveren.php
+++ reserveren.php
@@ -1,65 +1,29 @@
 <?php
+include 'functions.php';
 session_start();
 // Checken of de gebruiker is ingelogd. Als hij niet is ingelogd wordt hij teruggestuurd naar index.html
 if (!isset($_SESSION['loggedin'])) {
-	header('location: index.html');
-	exit;
+    header('location: index.html');
+    exit;
 }
-//Haal alle afspraken op van de huidige maand uit de databaase
-$mysqli = new mysqli("localhost", "root", "", "reserveringssysteem");
-$stmt = $mysqli->prepare("SELECT * FROM bookings where MONTH(date)=MONTH(now())
-       and YEAR(date)=YEAR(now()) ");
-//Maak een array van deze data met de while loop
-$bookings = array();
-//if statement maken die de resultaten ophaalt van de query.
-if($stmt->execute()){
-    $result = $stmt->get_result();
-    if($result->num_rows>0){
-        //associative array maken met datum.
-        while($row = $result->fetch_assoc()){
-            $bookings[] = $row['date'];
-        }
-        $stmt->close();
-    }
-}
-// Tijdzone neerzetten voor de zekerheid
-date_default_timezone_set('Europe/Amsterdam');
-//year en month uit de database halen
+
 if (isset($_GET['ym'])) {
     $ym = $_GET['ym'];
 } else {
     // data van deze maand
     $ym = date('Y-m');
 }
-
-// Check format
 $timestamp = strtotime($ym . '-01');
-//zorgen dat altijd de juiste timestamp wordt weergegeven
-if ($timestamp === false) {
-    $ym = date('Y-m');
-    $timestamp = strtotime($ym . '-01');
-}
-
-// dag van vandaag in de format Year-Month-Day.
-$today = date('Y-m-j', time());
-
-// titel voor bovenaan de kalender
-$html_title = date('F Y', $timestamp);
-
-// vorige, huidige en volgende maand berekenen met behulp van strtotime.
 $prev = date('Y-m', strtotime('-1 month', $timestamp));
 $current = date('Y-m');
 $next = date('Y-m', strtotime('+1 month', $timestamp));
+$html_title = date('F Y', $timestamp);
 
-// Dagen in een maand
-$day_count = date('t', $timestamp);
-
-// aantal dagen in de week
-$str = date('w', $timestamp);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>Reserveren</title>
@@ -82,46 +46,55 @@ $str = date('w', $timestamp);
         h3 {
             margin-bottom: 30px;
         }
+
         th {
             height: 30px;
             text-align: center;
         }
+
         td {
             height: 100px;
         }
-        .btn-danger{
-            color:#fff;
-            background-color:#c9302c;
-            background-image:none;
-            border-color:#ac2925;
+
+        .btn-danger {
+            color: #fff;
+            background-color: #c9302c;
+            background-image: none;
+            border-color: #ac2925;
             width: 80px;
         }
+
         .btn-success {
-            color:#fff;
-            background-color:#5cb85c;
-            border-color:#4cae4c;
+            color: #fff;
+            background-color: #5cb85c;
+            border-color: #4cae4c;
             width: 80px;
         }
-        .table-bordered td,.table-bordered th , .table-bordered tr{
-            border:7px solid #dba617 !important;
+
+        .table-bordered td,
+        .table-bordered th,
+        .table-bordered tr {
+            border: 7px solid #dba617 !important;
 
 
         }
+
         .table {
             border-collapse: collapse;
             border-radius: 1em;
             overflow: hidden;
         }
+
         .table-header {
-         background-color: #dba617;
+            background-color: #dba617;
             font-family: 'Oswald', sans-serif;
             font-size: 15px;
         }
-      
     </style>
 </head>
+
 <body>
-<div class="navbar">
+    <div class="navbar">
         <div class="navbar-logo">
             <a href="jumbo-intern.nl"><img src="https://wereldlichtjesdagleidscherijn.nl/wp-content/uploads/2018/11/Jumbo_Logo.svg_-1030x287.png" width="210px" height="70px"></img>
             </a>
@@ -157,70 +130,33 @@ $str = date('w', $timestamp);
             </a>
         </div>
     </div>
-<div class="container">
-  <table class="table table-bordered">
-    <th>
-    <center><h2 class="htmltitle"><?= htmlentities($html_title); ?></h2></center>
-    <center><h4><button class="intbutton"><a class="but" href="?ym=<?php echo $prev; ?>">Vorige</a></button>  <button class="intbutton"><a class="but" href="?ym=<?php echo $current; ?>">Heden</a></button>  <button class="intbutton"><a class="but" href="?ym=<?php echo $next; ?>">Volgende</a></h4></button></center>
-    </th>
-  </table>
-    <table class="table table-bordered">
+    <div class="container">
+        <table class="table table-bordered">
+            <th>
+                <center>
+                    <h2 class="htmltitle"><?= htmlentities($html_title); ?></h2>
+                </center>
+                <center>
+                    <h4><button class="intbutton"><a class="but" href="?ym=<?php echo $prev; ?>">Vorige</a></button> <button class="intbutton"><a class="but" href="?ym=<?php echo $current; ?>">Heden</a></button> <button class="intbutton"><a class="but" href="?ym=<?php echo $next; ?>">Volgende</a></h4></button>
+                </center>
+            </th>
+        </table>
+        <table class="table table-bordered">
 
-        <tr class="table-header">
-            <th>Zondag</th>
-            <th>Maandag</th>
-            <th>Dinsdag</th>
-            <th>Woensdag</th>
-            <th>Donderdag</th>
-            <th>Vrijdag</th>
-            <th>Zaterdag</th>
-        </tr>
-        <?php
-       //Kalender maken
-$weeks = array();
-$week = '';
-//vak maken voor elke dag van de week met string repeat (hierdoor wordt er voor elke dag van de week een leeg vak gemaakt met html)
-$week .= str_repeat('<td></td>', $str);
-//boekingen tellen
-$bookingsAmount = array_count_values($bookings);
-
-//for loop maken die voor elke dag in de maand een vakje maakt
-for ( $day = 1; $day <= $day_count; $day++, $str++) {
-    $date = $ym . '-' . $day;
-    $week .= '<td>' . $day;
-    //knoppen toevoegen als de datum van de dag groter of gelijk is aan de dag van vandaag.
-    if (strtotime($date) >= strtotime(date('Y-m-d'))){
-        if (!isset($bookingsAmount[$date]) || $bookingsAmount[$date] < 5) {
-            $week .= "<center><a href='book.php?date=".$date."' class='btn btn-success btn-xs'>Book</a></center>";
-        }
-        // Als er meer dan 5 reserveringen op die dag zijn is het vol.
-        else {
-            $week .= "<center><button class='btn btn-danger btn-xs'>Vol!</button></center>";
-
-        }
-    } 
-    $week .= '</td>';
-
-    // If statement maken die checkt of het het einde van de maand is en vervolgens een leeg vakje maakt voor de dagen die er nog in zouden passen. 
-    if ($str % 7 == 6 || $day == $day_count) {
-// % teken hierboven zorgt ervoor dat er gecontroleerd word dat de week geeindigd is. Het pakt de remainder nadat je de 2 getallen gedeeld hebt als dat uberhaupt kan.
-        if ($day == $day_count) {
-            // voeg leeg vak toe om geen 32,33 enzo te krijgen
-            $week .= str_repeat('<td></td>', 6 - ($str % 7));
-        }
-
-        $weeks[] = '<tr>' . $week . '</tr>';
-
-        // Nieuwe week aanmaken
-        $week = '';
-    }
-}
-//calender laten zien door de variabele week te echo'en
-       foreach ($weeks as $week) {
-            echo $week;
-        }
-        ?>
-    </table>
-</div>
+            <tr class="table-header">
+                <th>Zondag</th>
+                <th>Maandag</th>
+                <th>Dinsdag</th>
+                <th>Woensdag</th>
+                <th>Donderdag</th>
+                <th>Vrijdag</th>
+                <th>Zaterdag</th>
+            </tr>
+            <?php
+            return  calendar();
+            ?>
+        </table>
+    </div>
 </body>
+
 </html>
